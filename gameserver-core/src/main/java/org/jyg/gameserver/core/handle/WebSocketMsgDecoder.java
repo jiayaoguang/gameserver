fb17cf12173b75bc9a5d32355bc362c05678b0ec
jiayaoguang <1090196116@qq.com>
2022-02-14 23:39:38 +0800

    websocket

---
+++ gameserver-core/src/main/java/org/jyg/gameserver/core/handle/WebSocketMsgDecoder.java
@@ -0,0 +1,186 @@
+package org.jyg.gameserver.core.handle;
+
+import com.google.protobuf.MessageLite;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.*;
+import io.netty.handler.codec.http.websocketx.BinaryWebSocketFrame;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+import io.netty.handler.codec.http.websocketx.WebSocketFrame;
+import org.jyg.gameserver.core.enums.EventType;
+import org.jyg.gameserver.core.msg.AbstractMsgCodec;
+import org.jyg.gameserver.core.msg.ByteMsgObj;
+import org.jyg.gameserver.core.util.AllUtil;
+import org.jyg.gameserver.core.util.Context;
+import org.jyg.gameserver.core.util.Logs;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * created by jiayaoguang at 2022年2月4日
+ */
+public class WebSocketMsgDecoder extends
+		SimpleChannelInboundHandler<WebSocketFrame> {
+
+//	public static Set<Channel> channels = new HashSet<>();
+	private final Context context;
+
+	public WebSocketMsgDecoder(Context context) {
+		this.context = context;
+	}
+
+	@Override
+	public void channelActive(ChannelHandlerContext ctx) throws Exception { // (5)
+		Channel incoming = ctx.channel();
+		Logs.DEFAULT_LOGGER.info("Client:" + incoming.remoteAddress() + "在线");
+
+		context.getConsumerManager().publicEventToDefault(EventType.SOCKET_CONNECT_ACTIVE, null, ctx.channel() , 0 );
+		
+		super.channelActive(ctx);
+	}
+	
+	@Override
+	protected void channelRead0(ChannelHandlerContext ctx,
+			WebSocketFrame frame) throws Exception { // (1)
+		
+		//|| frame instanceof BinaryWebSocketFrame
+		if (frame == null) {
+			return;
+		}
+
+
+		ByteBuf msgbyteBuf ;
+
+
+		if(frame instanceof TextWebSocketFrame){
+			TextWebSocketFrame textWebSocketFrame = (TextWebSocketFrame)frame;
+//			msgBytes = textWebSocketFrame.text().getBytes(StandardCharsets.UTF_8);
+			msgbyteBuf = frame.content();
+		}else if(frame instanceof BinaryWebSocketFrame){
+			BinaryWebSocketFrame binaryWebSocketFrame = (BinaryWebSocketFrame)frame;
+
+			msgbyteBuf = frame.content();
+		}else {
+			Logs.DEFAULT_LOGGER.error(" webscoket frame type not support : {} ", frame.getClass().getSimpleName());
+			return;
+		}
+
+
+
+		try{
+
+			int msgId = msgbyteBuf.readInt();
+//            Logs.DEFAULT_LOGGER.debug("cnf:" + frame.refCnt());
+			AbstractMsgCodec<?> msgCodec = context.getMsgCodec(msgId);
+			if (msgCodec == null) {
+				Logs.DEFAULT_LOGGER.error(" protoParser not found ,id : {} ", msgId);
+				return;
+			}
+
+			int readableBytes = msgbyteBuf.readableBytes();
+
+
+			final byte[] dstBytes = new byte[msgbyteBuf.readableBytes()];;
+			if(readableBytes > 0){
+				msgbyteBuf.getBytes(msgbyteBuf.readerIndex(), dstBytes);
+			}else {
+//                LOGGER.info(" readableBytes <= 0 ,id : {} ", msgId);
+			}
+
+			Object msgObj;
+
+			try{
+				msgObj = msgCodec.decode(dstBytes);
+			}catch (Exception e){
+				Logs.DEFAULT_LOGGER.error(" msg decode make exception, msg type : {}  , exception {}", msgCodec.getMsgType(), e.getCause());
+				throw e;
+			}
+
+			switch (msgCodec.getMsgType()) {
+				case PROTO:
+					MessageLite messageLite = (MessageLite) msgObj;
+					context.getConsumerManager().publicEventToDefault( EventType.PROTO_MSG_COME, messageLite, ctx.channel(), msgId);
+					break;
+				case BYTE_OBJ:
+					ByteMsgObj byteMsgObj = (ByteMsgObj) msgObj;
+					context.getConsumerManager().publicEventToDefault(EventType.BYTE_OBJ_MSG_COME, byteMsgObj, ctx.channel(), msgId);
+					break;
+				default:
+					Logs.DEFAULT_LOGGER.error(" unknown msg type type : {} ", msgCodec.getMsgType());
+					break;
+			}
+
+
+		}catch (Exception e){
+			e.printStackTrace();
+			final String addrRemote = AllUtil.getChannelRemoteAddr(ctx.channel());
+			ctx.channel().close().addListener(new ChannelFutureListener() {
+				@Override
+				public void operationComplete(ChannelFuture future) throws Exception {
+					Logs.DEFAULT_LOGGER.info("closeChannel: close the connection to remote address[{}] result: {}", addrRemote,
+							future.isSuccess());
+				}
+			});
+		} finally {
+
+//            Logs.DEFAULT_LOGGER.info("frame.refCnt() : " + frame.refCnt());
+
+		}
+		
+	}
+	
+	
+	@Override
+	public void channelInactive(ChannelHandlerContext ctx) throws Exception { // (6)
+		Channel incoming = ctx.channel();
+		Logs.DEFAULT_LOGGER.info("Client:" + incoming.remoteAddress() + "掉线");
+
+		context.getConsumerManager().publicEventToDefault(EventType.SOCKET_CONNECT_INACTIVE, null, ctx.channel() , 0);
+		
+		super.channelInactive(ctx);
+	}
+	
+	//------------------------start-----------------------------
+	public static Map<String,String> toMap(String text){
+		Map<String,String> map = new HashMap<>();
+		for(String keyValue:text.split("&")) {
+			int index = keyValue.indexOf('=');
+			if(index<0) {
+				continue;
+			}
+			map.put(keyValue.substring(0,index), keyValue.substring(index+1,keyValue.length()));
+		}
+		return map;
+	}
+	
+	
+	//-------------------------end---------------------------------------
+
+	@Override
+	public void handlerAdded(ChannelHandlerContext ctx) throws Exception { // (2)
+		super.handlerAdded(ctx);
+//		Channel incoming = ctx.channel();
+//		for (Channel channel : channels) {
+//			channel.writeAndFlush(new TextWebSocketFrame("[SERVER] - "
+//					+ incoming.remoteAddress() + " 加入"));
+//		}
+//		channels.add(ctx.channel());
+//		System.out.println("Client:" + incoming.remoteAddress() + "加入");
+		Logs.DEFAULT_LOGGER.info("add");
+	}
+
+	@Override
+	public void handlerRemoved(ChannelHandlerContext ctx) throws Exception { // (3)
+		super.handlerRemoved(ctx);
+//		Channel incoming = ctx.channel();
+//		for (Channel channel : channels) {
+//			channel.writeAndFlush(new TextWebSocketFrame("[SERVER] - "
+//					+ incoming.remoteAddress() + " 离开"));
+//		}
+//		System.out.println("Client:" + incoming.remoteAddress() + "离开");
+//		channels.remove(ctx.channel());
+		Logs.DEFAULT_LOGGER.info("remove");
+	}
+
+
+}
---
+++ gameserver-core/src/main/java/org/jyg/gameserver/core/handle/WebSocketMsgEncoder.java
@@ -0,0 +1,115 @@
+package org.jyg.gameserver.core.handle;
+
+import com.google.protobuf.MessageLite;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelPromise;
+import io.netty.handler.codec.EncoderException;
+import io.netty.handler.codec.MessageToByteEncoder;
+import io.netty.handler.codec.http.websocketx.BinaryWebSocketFrame;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+import io.netty.handler.codec.http.websocketx.WebSocketFrame;
+import io.netty.util.ReferenceCountUtil;
+import org.jyg.gameserver.core.msg.AbstractMsgCodec;
+import org.jyg.gameserver.core.msg.ByteMsgObj;
+import org.jyg.gameserver.core.util.AllUtil;
+import org.jyg.gameserver.core.util.Context;
+import org.jyg.gameserver.core.util.Logs;
+
+/**
+ * create by jiayaoguang on 2022/2/4
+ */
+public class WebSocketMsgEncoder extends MessageToByteEncoder<Object> {
+
+    protected final Context context;
+
+    public WebSocketMsgEncoder(Context context) {
+        this.context = context;
+    }
+
+
+    public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
+        WebSocketFrame webSocketFrame = null;
+
+        try {
+
+
+            if (msg instanceof ByteMsgObj) {
+                ByteMsgObj cast = (ByteMsgObj) msg;
+
+                Class<? extends ByteMsgObj> byteMsgObjClazz = cast.getClass();
+//        Logs.DEFAULT_LOGGER.info("deal threadName : " + Thread.currentThread().getName());
+
+                int eventId = context.getMsgIdByByteMsgObj(byteMsgObjClazz);
+                if (eventId <= 0) {
+                    Logs.DEFAULT_LOGGER.error("unknow eventid ,class {}" + byteMsgObjClazz.getSimpleName());
+                    throw new IllegalArgumentException("unknow eventid "+ byteMsgObjClazz.getSimpleName());
+                }
+
+
+                AbstractMsgCodec msgCodec = context.getMsgCodec(eventId);
+
+
+                if(msgCodec == null){
+                    Logs.DEFAULT_LOGGER.error("unknow msgCodec ,class {}" + byteMsgObjClazz.getSimpleName());
+                    throw new IllegalArgumentException("unknow msgCodec "+ byteMsgObjClazz.getSimpleName());
+                }
+
+                byte[] msgBytes = msgCodec.encode(cast);
+
+                webSocketFrame = new BinaryWebSocketFrame(Unpooled.wrappedBuffer(msgBytes));
+
+                ctx.write(webSocketFrame, promise);
+
+                webSocketFrame = null;
+            } else if (msg instanceof MessageLite) {
+                MessageLite cast = (MessageLite) msg;
+
+                Class<? extends MessageLite> protoClass = cast.getClass();
+//        Logs.DEFAULT_LOGGER.info("deal threadName : " + Thread.currentThread().getName());
+
+                int eventId = context.getMsgIdByProtoClass(protoClass);
+                if (eventId <= 0) {
+                    Logs.DEFAULT_LOGGER.error("unknow eventid ,class {}" + protoClass.getSimpleName());
+                    throw new IllegalArgumentException("unknow eventid "+ protoClass.getSimpleName());
+                }
+
+
+                AbstractMsgCodec msgCodec = context.getMsgCodec(eventId);
+
+
+                if(msgCodec == null){
+                    Logs.DEFAULT_LOGGER.error("unknow msgCodec ,class {}" + protoClass.getSimpleName());
+                    throw new IllegalArgumentException("unknow msgCodec "+ protoClass.getSimpleName());
+                }
+
+                byte[] msgBytes = msgCodec.encode(cast);
+
+                webSocketFrame = new BinaryWebSocketFrame(Unpooled.wrappedBuffer((msgBytes)));
+
+                ctx.write(webSocketFrame, promise);
+
+                webSocketFrame = null;
+            } else {
+                ctx.write(msg, promise);
+            }
+        } catch (EncoderException var17) {
+            throw var17;
+        } catch (Throwable var18) {
+            throw new EncoderException(var18);
+        } finally {
+            if (webSocketFrame != null) {
+                webSocketFrame.release();
+            }
+
+        }
+
+    }
+
+    @Override
+    protected void encode(ChannelHandlerContext channelHandlerContext, Object o, ByteBuf byteBuf) throws Exception {
+
+    }
+
+}
