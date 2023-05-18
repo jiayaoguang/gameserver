a665af6f4bed0aecf40e9d8f383983091f2e42a4
jiayaoguang <1090196116@qq.com>
2023-05-18 12:38:12 +0800

    IntervalUpdateQueueConsumerThread 修改

--- gameserver-core/src/main/java/org/jyg/gameserver/core/consumer/IntervalUpdateQueueConsumerThread.java
+++ gameserver-core/src/main/java/org/jyg/gameserver/core/consumer/IntervalUpdateQueueConsumerThread.java
@@ -1,11 +1,14 @@
 package org.jyg.gameserver.core.consumer;
 
+import org.jyg.gameserver.core.util.Logs;
+
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.LockSupport;
 
 /**
  * create by jiayaoguang on 2023/4/8
  * 指定循环时间间隔的消费者线程
+ * 实际执行的时间间隔会大于等于传入的时间间隔
  */
 public class IntervalUpdateQueueConsumerThread extends QueueConsumerThread {
 
@@ -32,19 +35,22 @@ public class IntervalUpdateQueueConsumerThread extends QueueConsumerThread {
 
     @Override
     protected void beforeUpdate() {
-        lastUpdateNanoTime = System.nanoTime();
+
+
     }
 
     @Override
     protected void afterUpdateApplyWait() {
         long curNano = System.nanoTime();
         long nextUpdateNano = lastUpdateNanoTime + updateIntervalNano;
+
+
         if(nextUpdateNano > curNano){
             LockSupport.parkNanos(nextUpdateNano - curNano);
         }else {
             Thread.yield();
         }
-        lastUpdateNanoTime = curNano;
+        lastUpdateNanoTime = System.nanoTime();
     }
 
 
